373b739347f739a1744c30dfb2da0c49b46a2dd2
LayneKM <laynemikan@gmail.com>
2020-03-17 17:11:47 -0400

    Implement more of the hand detection algorithm

--- clockDetection.py
+++ clockDetection.py
@@ -8,7 +8,7 @@ cannyLowerThreshold = 100
 cannyUpperThreshold = 200
 
 houghLinesThreshold = 90
-houghLinesMinLineLength = 1
+houghLinesMinLineLength = 5
 houghLinesMaxLineGap = 10
 
 # This function is for detecting the clock's outer circumference using Hough Transform
@@ -50,14 +50,27 @@ def detectClockHands(clockImg):
         d = abs(((x2 - x1) * c[0]) + ((y1 - y2) * c[1]) + ((x1 - x2) * y1) + ((y2 - y1) * x1)) / math.sqrt(math.pow((x2 - x1), 2) + math.pow((y1 - y2), 2))
         if d <= r:
             goodLines.append(line)
-            cv.line(clockImg, (x1, y1), (x2, y2), (0, 255, 0), 2)
 
-    print(goodLines)
+    # Merge nearby lines together so there is not 2 lines for each hand
+    # They will be turned into one line, located at the midpoint between the two lines
+    # TODO: Probably need to track the thickness of the hand since the length of hour and minute hands are often similar
+    mergedLines = []
+    d = h // 30
+    for i in range(0, len(goodLines)):
+        for j in range(i + 1, len(goodLines)):
+            ix1, iy1, ix2, iy2 = goodLines[i][0]
+            jx1, jy1, jx2, jy2 = goodLines[j][0]
+            if (abs(ix1 - jx1) < d and abs(iy1 - jy1) < d and abs(ix2 - jx2) < d and abs(iy2 - jy2) < d):
+                mergedLines.append([(ix1 + jx1) // 2, (iy1 + jy1) // 2, (ix2 + jx2) // 2, (iy2 + jy2) // 2]) 
+
+    # TODO: DISPLAY FOR TESTING PURPOSES
+    for line in mergedLines:
+        x1, y1, x2, y2 = line
+        cv.line(clockImg, (x1, y1), (x2, y2), (0, 255, 0), 2)
+
     return clockImg
 
-# Calling the hough detection function
 # Passes image file into the function through the command line arguments
-# TODO: FOR TESTING PURPOSES ONLY, TO BE REMOVED LATER
 clockImg = cv.imread("images/" + sys.argv[1])
 isolatedImg = isolateClock(clockImg)
 hands = detectClockHands(isolatedImg)
