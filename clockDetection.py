cf5d47dd7cd0b74717d1c22cf22c069d435f65db
LayneKM <laynemikan@gmail.com>
2020-03-17 16:39:52 -0400

    Update to use HoughLinesP because it gives the actual start/end coords of the lines

--- clockDetection.py
+++ clockDetection.py
@@ -3,9 +3,13 @@ import cv2 as cv
 import numpy as np
 import math
 
+# A lot of these values are arbitary based on images that were tested
 cannyLowerThreshold = 100
 cannyUpperThreshold = 200
+
 houghLinesThreshold = 90
+houghLinesMinLineLength = 1
+houghLinesMaxLineGap = 10
 
 # This function is for detecting the clock's outer circumference using Hough Transform
 # Recieves image as input and returns the same image cropped around the circle
@@ -25,46 +29,35 @@ def isolateClock(clockImg):
     return gray[(y-r):(y+r), (x-r):(x+r)]
 
 def detectClockHands(clockImg):
+    # TODO: FOR TESTING PURPOSES ONLY (ie. drawing lines in bright green)
+    clockImg = cv.cvtColor(clockImg, cv.COLOR_GRAY2BGR)
+
+    # Extract edges from image
     edges = cv.Canny(clockImg, cannyLowerThreshold, cannyUpperThreshold)
 
-    lines = cv.HoughLines(edges, 1, np.pi / 180, houghLinesThreshold)
-
-    # Remove lines that do not pass through center (ie. are not clock hands)
     h = clockImg.shape[0]
     w = clockImg.shape[1]
     c = (h // 2, w // 2)
-
-    # Center radius that clock hands should pass through
-    # This size is relative to the size of the image
     r = h // 30
 
-    # Source: https://docs.opencv.org/3.4/d9/db0/tutorial_hough_lines.html
+    # Detect lines in image
+    lines = cv.HoughLinesP(edges, 1, np.pi / 180, houghLinesThreshold, None, houghLinesMinLineLength, houghLinesMaxLineGap)
+
+    # Remove lines which do not pass through the center radius
     goodLines = []
     for line in lines:
-        rho = line[0][0]
-        theta = line[0][1]
-
-        a = np.cos(theta)
-        b = np.sin(theta)
-        x0 = a * rho
-        y0 = b * rho
-        x1 = int(x0 + w * b * -1)
-        y1 = int(y0 + h * a)
-        x2 = int(x0 - w * b * -1)
-        y2 = int(y0 - h * a)
-
-        # Add line to goodLines if it falls within the center's radius
-        # Source: https://math.stackexchange.com/questions/275529/check-if-line-intersects-with-circles-perimeter
+        x1, y1, x2, y2 = line[0]
         d = abs(((x2 - x1) * c[0]) + ((y1 - y2) * c[1]) + ((x1 - x2) * y1) + ((y2 - y1) * x1)) / math.sqrt(math.pow((x2 - x1), 2) + math.pow((y1 - y2), 2))
         if d <= r:
             goodLines.append(line)
-            cv.line(clockImg, (x1, y1), (x2, y2), (0, 0, 255), 2)
+            cv.line(clockImg, (x1, y1), (x2, y2), (0, 255, 0), 2)
 
+    print(goodLines)
     return clockImg
 
 # Calling the hough detection function
 # Passes image file into the function through the command line arguments
-# FOR TESTING PURPOSES ONLY, TO BE REMOVED LATER
+# TODO: FOR TESTING PURPOSES ONLY, TO BE REMOVED LATER
 clockImg = cv.imread("images/" + sys.argv[1])
 isolatedImg = isolateClock(clockImg)
 hands = detectClockHands(isolatedImg)
